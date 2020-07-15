ad625feb9b290a7d697e6d217ca3fc8607805325
Yann Buydens Speech Act <yannbuydensspeechact@MacBook-Pro.local>
2020-07-15 18:24:11 +0200

    added import

--- src/lambda/graphql.js
+++ src/lambda/graphql.js
@@ -1,6 +1,6 @@
 // example of async handler using async-await
 // https://github.com/netlify/netlify-lambda/issues/43#issuecomment-444618311
-
+import { ApolloServer, gql } from 'apollo-server-lambda'
 import axios from "axios"
 export async function handler(event, context) {
   try {
