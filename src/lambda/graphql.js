00e0b16baab44ff642094ee38ab16c30663ba041
Yann Buydens Speech Act <yannbuydensspeechact@MacBook-Pro.local>
2020-07-15 17:42:58 +0200

    wtf

--- src/lambda/graphql.js
+++ src/lambda/graphql.js
@@ -1,23 +1,44 @@
-const { ApolloServer, gql } = require("apollo-server-lambda")
+// example of async handler using async-await
+// https://github.com/netlify/netlify-lambda/issues/43#issuecomment-444618311
 
-
-const typeDefs = gql`
-    type Query {
-        hello: String
+import axios from "axios"
+export async function handler(event, context) {
+  try {
+    const response = await axios.get("https://icanhazdadjoke.com", { headers: { Accept: "application/json" } })
+    const data = response.data
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ msg: data.joke })
     }
-`;
-
-const resolvers = {
-  Query: {
-    hello: (parent, args, context) => {
-      return "Hello, world!";
+  } catch (err) {
+    console.log(err) // output to netlify function log
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e. JSON.stringify(err)
     }
   }
-};
+}
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-});
-
-exports.handler = server.createHandler();
+// const { ApolloServer, gql } = require("apollo-server-lambda")
+//
+//
+// const typeDefs = gql`
+//     type Query {
+//         hello: String
+//     }
+// `;
+//
+// const resolvers = {
+//   Query: {
+//     hello: (parent, args, context) => {
+//       return "Hello, world!";
+//     }
+//   }
+// };
+//
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers
+// });
+//
+// exports.handler = server.createHandler();
